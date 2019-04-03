a1392d67db5ed291afe182c808003c1994005789
CGuijarro <cesarguijarro@gmail.com>
2019-04-03 18:05:27 -0500

    One more time

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d35f4ccc-569a-49f5-acb2-f2176a4dadbc</testSuiteGuid>
+   <testCaseLink>
+      <guid>0690891c-f624-4f4e-a7ff-d0906886b56f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase-1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1e20b63c-78b2-4e3a-8f5f-d2cbecbab14f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
